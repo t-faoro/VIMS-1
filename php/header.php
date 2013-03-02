c4a82514d347c426ef6d757180127f6f48da39ab
Tylor Faoro <t.d.g.faoro@gmail.com>
2013-03-02 01:22:19 -0700

    Removed Unnecessary Login Class, updated Header/Footer/Stylesheet so that values would stay in their homes
    Items were moving out of their cells when the page was resized, so I set
    things into the appropriate cells, altered a few things, and now things
    SHOULD all stay in their homes

--- php/header.php
+++ php/header.php
@@ -1,10 +1,9 @@
 <?php
-	require_once "AssistingFunctions.php";
 	echo '<div id="header_container" >';   
     
 	echo '<div id="topLeft">'.IMG("logo_clubwatch_v4.1.png", "Clubwatch Logo").'<p>Powered By: <span class="yellow">Clubwatch</span></p>'.'</div>';
     echo '<div id="topMiddle"><h2>Venue Information Management System</h2></div>';    
-	echo '<div id="topRight"><p>Welcome to VIMS, Please Log In</p></div>';
+	echo '<div id="topRight"><p>Please Log In</p></div>';
 	
 	echo '</div>';
 ?>
