a6a01096a897e1223a73ab6b03480be189915ae5
Justin Werre <werrejustin@gmail.com>
2013-03-01 18:41:14 -0700

    Finished fixing html syntax issues

--- php/footer.php
+++ php/footer.php
@@ -1,9 +1,11 @@
 <?php
-echo '<div id="footer_container">';
+echo "<div id='footer_container'>\n";
     
-    echo '<div id="bottomLeft"><p>Developed By: <span class="yellow">Excelsior Systems</span></p><p>&#169; 2013 Excelsior Systems <br /> All Rights Reserved</p></div>';
-    echo '<div id="bottomMiddle"></div>';
-    echo '<div id="bottomRight">&nbsp;</div>';
+    echo "<div id='bottomLeft'><p>Developed By: <span class='yellow'>Excelsior Systems</span></p><p>&#169; 2013 Excelsior Systems <br /> All Rights Reserved</p></div>\n";
+    echo "<div id='bottomMiddle'></div>\n";
+    echo "<div id='bottomRight'>&nbsp;</div>\n";
 
-echo '</div>';
+echo "</div>\n";
+echo "</body>\n";
+echo "</html>";
 ?>
--- php/header.php
+++ php/header.php
@@ -1,10 +1,11 @@
 <?php
 	require_once "AssistingFunctions.php";
+	echo "<body>\n";
 	echo "<div id='header_container' >\n";   
     
-	echo "<div id='topLeft'>".IMG("logo_clubwatch_v4.1.png", "Clubwatch Logo")."<p>Powered By: <span class='yellow'>Clubwatch</span></p>\n"."</div>";
-    echo '<div id="topMiddle"><h2>Venue Information Management System</h2></div>';    
-	echo '<div id="topRight"><p>Welcome to VIMS, Please Log In</p></div>';
+	echo "<div id='topLeft'>".IMG("logo_clubwatch_v4.1.png", "Clubwatch Logo")."<p>Powered By: <span class='yellow'>Clubwatch</span></p>\n"."</div>\n";
+    echo "<div id='topMiddle'><h2>Venue Information Management System</h2></div>\n";    
+	echo "<div id='topRight'><p>Welcome to VIMS, Please Log In</p></div>\n";
 	
-	echo '</div>';
+	echo "</div>\n";
 ?>
